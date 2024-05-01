e01efcd3e9ede260c1ef88a7415a287f647ff099
FranciscoJSB12 <javif8@gmail.com>
2024-05-01 14:50:51 -0400

    docs: Add instructions to run the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      //Autoloadentities es para que cargue las entidades que vamos definiendo poco a poco
+      autoLoadEntities: true,
+      synchronize: true
+      //En producción generalmente este valor se deja en falso (synchronize)
+      //también se puede crear una variable de entorno para manejar la sincronización
+    })
+  ],
 })
 export class AppModule {}
