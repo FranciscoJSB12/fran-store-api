331a8f7dae822d0c861cbdc0ad74b91cb35137a1
FranciscoJSB12 <javif8@gmail.com>
2024-05-01 15:53:22 -0400

    feat: Add product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,35 @@
-export class CreateProductDto {}
+import { IsArray, IsIn, IsInt, IsNumber, IsOptional, IsPositive, IsString, MinLength } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title: string;
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?: number;
+
+    @IsString()
+    @IsOptional()
+    description?: string;
+
+    @IsString()    
+    @IsOptional()
+    slug?: string;
+
+    @IsInt()
+    //Nota: la diferencia entre este y el number es que IsInt no acepta decimales
+    @IsPositive()
+    @IsOptional()
+    stock?: number;
+
+    @IsString({ each: true })
+    //Nota: el each: true quiere decir que cada elemento en el arreglo debe cumplir con ser un string
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(['men', 'women', 'kid', 'unisex'])
+    gender: string;
+}
