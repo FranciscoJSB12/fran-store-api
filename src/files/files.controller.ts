d52b646674fced87add2663311fd7fe8377b7623
FranciscoJSB12 <javif8@gmail.com>
2024-05-02 15:50:02 -0400

    feat: return secure url in file controller

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,5 +1,6 @@
 import { Response } from 'express';
 import { BadRequestException, Controller, Res, Get, Param, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { FilesService } from './files.service';
@@ -8,7 +9,10 @@ import { fileNamer } from './helpers/fileNamer.helper';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) {}
 
   @Get('product/:imageName')
   findProductImage (
@@ -42,7 +46,7 @@ export class FilesController {
         throw new BadRequestException('Make sure that the file is an image');
       }
 
-      const secureUrl = `${file.filename}`;
+      const secureUrl = `${this.configService.get('HOST_API')}/files/product/${file.filename}`;
 
       return {
         secureUrl
