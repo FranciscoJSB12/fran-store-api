8f3efecaa7a6a814cae7ee6e0d37664fc511a0ea
FranciscoJSB12 <javif8@gmail.com>
2024-05-03 11:21:56 -0400

    feat: Insert userId in products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -2,9 +2,10 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, Query
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { PaginationDto } from 'src/common/dto/pagination.dto';
-import { Auth } from '../auth/decorators';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { Auth, GetUser } from '../auth/decorators';
 import { ValidRoles } from '../auth/interfaces/valid-roles';
+import { User } from '../auth/entities/user.entity';
 
 @Controller('products')
 //@Auth()
@@ -14,8 +15,11 @@ export class ProductsController {
 
   @Post()
   @Auth(ValidRoles.admin)
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(createProductDto);
+  create(
+    @Body() createProductDto: CreateProductDto,
+    @GetUser() user: User
+  ) {
+    return this.productsService.create(createProductDto, user);
   }
 
   @Get()
@@ -32,8 +36,10 @@ export class ProductsController {
   @Auth(ValidRoles.admin)
   update(
     @Param('id', ParseUUIDPipe) id: string, 
-    @Body() updateProductDto: UpdateProductDto) {
-    return this.productsService.update(id, updateProductDto);
+    @Body() updateProductDto: UpdateProductDto,
+    @GetUser() user: User,
+  ) {
+    return this.productsService.update(id, updateProductDto, user);
   }
 
   @Delete(':id')
