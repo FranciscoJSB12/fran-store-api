719421b0dafd196c727069099edeedc2d6920059
FranciscoJSB12 <javif8@gmail.com>
2024-05-03 17:53:58 -0400

    feat: Implement JWT validation from handshake

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,22 +1,34 @@
 import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { JwtService } from '@nestjs/jwt';
 import { MessagesWsService } from './messages-ws.service';
 import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtPayload } from '../auth/interfaces/jwt-payload.interface';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
   @WebSocketServer() wss: Server;
   
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService
+  ) {}
 
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
     //console.log({ conectados: this.messagesWsService.getConnectedClients()});
-    const token = client.handshake.headers.authentication; //IMPORTANTE: la variable token almacenará los headers que mandemos desde el cliente, por ello hay que ver el nombre que se le dio en el cliente para poder desestructurarlo, en este caso fue authentication.
+    const token = client.handshake.headers.authentication as string; //IMPORTANTE: la variable token almacenará los headers que mandemos desde el cliente, por ello hay que ver el nombre que se le dio en el cliente para poder desestructurarlo, en este caso fue authentication.
+    let payload: JwtPayload;
 
-    console.log(token);
-
+    try {
+      payload = this.jwtService.verify(token);
+      //jwtService.verify nos da el payload del token
+    } catch (err) { 
+      client.disconnect();
+      return;
+    }
+    
     //clients-updated es el nombre del evento, puede ser cualquier string, lo segundo es el lo que se quiere mandar al cliente
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
