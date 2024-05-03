736e7a24e3bcb3f9401064aca295065bac5bbd49
FranciscoJSB12 <javif8@gmail.com>
2024-05-03 09:10:45 -0400

    feat: Create functionality for checking user role

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,7 @@
 import { Reflector } from '@nestjs/core';
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -17,9 +18,24 @@ export class UserRoleGuard implements CanActivate {
 
     const validRoles: string[] = this.reflector.get('roles', context.getHandler());
 
-    console.log({ validRoles });
+    if(!validRoles || validRoles.length === 0) return true;
+
+    const req = context.switchToHttp().getRequest();
+
+    const user = req.user as User;
+
+    if(!user) throw new BadRequestException('User not found');
+    /*IMPORTANTE, el AuthGuard() que está en el controlador es el que establce el usuario en los
+    headers */
+
+    for (const role of user.roles) {
+      if(validRoles.includes(role)) {
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(`User ${user.fullName} needs a valid role: [${validRoles}]`);
 
     /*Hay que estar pendiente de lo que regresa, si regresa false entonces la respuesta es 403 forbidden */
-    return true;
   }
 }
