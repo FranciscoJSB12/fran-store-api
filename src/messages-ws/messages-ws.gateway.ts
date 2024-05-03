1ef2fed0add695c63ce7df2e8bf645f321a32765
FranciscoJSB12 <javif8@gmail.com>
2024-05-03 16:45:32 -0400

    feat: Implement handle message from client

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,17 +1,31 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Socket, Server } from 'socket.io';
 import { MessagesWsService } from './messages-ws.service';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  
+  @WebSocketServer() wss: Server;
+  
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
     this.messagesWsService.registerClient(client);
     //console.log({ conectados: this.messagesWsService.getConnectedClients()});
+
+    //clients-updated es el nombre del evento, puede ser cualquier string, lo segundo es el lo que se quiere mandar al cliente
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
     this.messagesWsService.removeClient(client.id);
+    //Igualmente, cuando ocurre una desconexión se quiere escuchar
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient (client: Socket, payload: NewMessageDto) {
+    console.log(client.id, payload);
   }
 }
