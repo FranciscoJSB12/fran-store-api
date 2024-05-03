01ef98de7d356086eb7ccb9615f86beebac0b8dc
FranciscoJSB12 <javif8@gmail.com>
2024-05-03 11:56:13 -0400

    feat: Prepare seed with users, products and images

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,14 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
-import { Auth } from 'src/auth/decorators';
-import { ValidRoles } from 'src/auth/interfaces/valid-roles';
+//import { Auth } from 'src/auth/decorators';
+//import { ValidRoles } from 'src/auth/interfaces/valid-roles';
 
 @Controller('seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
   @Get()
-  @Auth(ValidRoles.admin)
+  //@Auth(ValidRoles.admin)
   /*IMPORTANTE: hay que tener el cuenta que el decorador
   auth usa el AuthGuard() está conectado a passport
   y passport es un módulo por lo que todo se encuentra encapsulado
